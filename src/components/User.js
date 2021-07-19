c8c3a850ea98e175bd812292ff1bc63608fc1349
Jakob <me@jterhorst.de>
2021-07-19 20:40:26 +0200

    Add solution exercise 4

--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,18 @@
-function User() {}
+import "./User.css";
+
+function User({ data }) {
+  return data.map((person) => {
+    return (
+      <div
+        className={`
+          person ${person.gender === "female" ? "female" : "male"} `}
+      >
+        <img src={person.picture.medium} alt={`${person.name.first}`} />
+
+        <p>{`This is: ${person.name.title} ${person.name.first} ${person.name.last}`}</p>
+      </div>
+    );
+  });
+}
 
 export default User;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,3 +1,43 @@
-function UserList() {}
+import { useState, useEffect } from "react";
+import User from "./User";
+import "./UserList.css";
+function UserList() {
+  const [people, setPeople] = useState([]);
+  const [number, setNumber] = useState(10);
+  const [gender, setGender] = useState("");
+
+  useEffect(() => {
+    const url = `https://randomuser.me/api/?inc=email,gender,name,picture&results=${number}&gender=${gender}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPeople(data.results));
+  }, [number, gender]);
+
+  function handleNumber(event) {
+    setNumber(event.target.value);
+  }
+
+  function handleSelect(event) {
+    const value = event.target.value;
+    const clean = value.toLowerCase();
+    setGender(clean);
+  }
+  return (
+    <div>
+      <header className="App-header">
+        <form>
+          <input type="number" min="1" max="30" onChange={handleNumber}></input>
+          <select className="selection" onChange={handleSelect}>
+            <option>All</option>
+            <option>Female</option>
+            <option>Male</option>
+          </select>
+        </form>
+      </header>
+      <main>{people && <User data={people} />}</main>
+    </div>
+  );
+}
 
 export default UserList;
